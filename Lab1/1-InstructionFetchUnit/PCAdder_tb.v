a661dfb28ab3f1ddb5973a4faf1f448fa19be6be
Brandon Sisco <166445283+Brhando@users.noreply.github.com>
2025-09-03 13:39:57 -0700

    Update PCAdder_tb.v
    Wrote initial test bench for PCAdder.

--- Lab1/1-InstructionFetchUnit/PCAdder_tb.v
+++ Lab1/1-InstructionFetchUnit/PCAdder_tb.v
@@ -13,15 +13,24 @@ module PCAdder_tb();
 
     wire [31:0] PCAddResult;
 
+	integer i; //declare i here; used in the for loop below
+
     PCAdder u0(
         .PCResult(PCResult), 
         .PCAddResult(PCAddResult)
     );
-
+	
 	initial begin
 	
-    /* Please fill in the implementation here... */
-	
+		PCResult = 32'd0; // set initially to 0
+		
+		for(i = 0; i < 10; i = i + 1) begin //walk through 10 values (0, 4, 8, etc.)
+			//#0 is enough => 1 delta cycle, and the chip is purely combinational
+			#0 $display("PC = %0d PC+4 = %0d", PCResult, PCAddResult); //set pcresult to 0 => should add 4 (PCAddResult should = 4) $display shows result
+			PCResult = PCResult + 32'd4; //increment by 4
+		end
+		$finish;
+		
 	end
 
 endmodule
