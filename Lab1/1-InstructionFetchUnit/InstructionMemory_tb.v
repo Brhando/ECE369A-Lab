4f4aba1713a6a64056b5d5531676f9f7779e290c
Brandon Sisco <166445283+Brhando@users.noreply.github.com>
2025-09-08 11:28:21 -0700

    Update InstructionMemory_tb.v
    Quick fixes

--- Lab1/1-InstructionFetchUnit/InstructionMemory_tb.v
+++ Lab1/1-InstructionFetchUnit/InstructionMemory_tb.v
@@ -13,6 +13,8 @@ module InstructionMemory_tb();
 
     reg [31:0] Address;
 
+	reg [31:0] expects;
+
 	InstructionMemory u0(
 		.Address(Address),
         .Instruction(Instruction)
@@ -21,21 +23,23 @@ module InstructionMemory_tb();
 	//define a task to test Address and Instruction at multiple values
 	task check(input [31:0] address_bytes);
 		begin
-			Address = adress_bytes; #1; //wait a cycle
-			$display("Address = %0d Index = %0d Instruction = %0d", Address, Address[8:2], Instruction);
+			Address = address_bytes; #1; //wait a cycle
+			expects = Address[8:2] * 32'd3; //expected value; mem[i] = i * 3;
+			$display("Address = %0d Index = %0d Instruction = %0d Expected Value = %0d", Address, Address[8:2], Instruction, expects);
 		end
 	endtask
 	
 	initial begin
 
 		//utilize the task to check Instruction at multiple values of Address; Address is byte index, so we use multiples of 4
-		check(32'd0); //index 0 - 3
-		check(32'd4); //index 4 - 7
-		check(32'd8);
+		check(32'd0); //index 0; bytes 0 - 3
+		check(32'd4); //index 1; bytes 4 - 7
+		check(32'd8); // etc...
 		check(32'd12);
 		check(32'd16);
 		check(32'd20);
 		check(32'd40);
+		$finish;
 		
 	end
 
