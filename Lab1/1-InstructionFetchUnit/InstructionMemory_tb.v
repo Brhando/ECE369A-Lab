93b409e455e54d36677503c89adcf1da1783ab00
Brandon Sisco <166445283+Brhando@users.noreply.github.com>
2025-09-08 11:15:18 -0700

    Update InstructionMemory_tb.v
    Wrot test bench for Instruction Memory

--- Lab1/1-InstructionFetchUnit/InstructionMemory_tb.v
+++ Lab1/1-InstructionFetchUnit/InstructionMemory_tb.v
@@ -18,10 +18,25 @@ module InstructionMemory_tb();
         .Instruction(Instruction)
 	);
 
+	//define a task to test Address and Instruction at multiple values
+	task check(input [31:0] address_bytes);
+		begin
+			Address = adress_bytes; #1; //wait a cycle
+			$display("Address = %0d Index = %0d Instruction = %0d", Address, Address[8:2], Instruction);
+		end
+	endtask
+	
 	initial begin
-	
-    /* Please fill in the implementation here... */
-	
+
+		//utilize the task to check Instruction at multiple values of Address; Address is byte index, so we use multiples of 4
+		check(32'd0); //index 0 - 3
+		check(32'd4); //index 4 - 7
+		check(32'd8);
+		check(32'd12);
+		check(32'd16);
+		check(32'd20);
+		check(32'd40);
+		
 	end
 
 endmodule
