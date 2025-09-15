643aacf81f9b9ddbb5044ffc1e57a419f4dc72fa
GriffithW <149918731+GriffithW@users.noreply.github.com>
2025-09-14 18:10:16 -0700

    Adding percent effort

--- Lab2(self-paced)/Sisco/2-debug/debug.s
+++ Lab2(self-paced)/Sisco/2-debug/debug.s
@@ -3,6 +3,10 @@
 #
 # Students: 
 #
+# Griffith 33%
+# Brandon 33% 
+# Gavin 33%
+#
 # 'count_occurence.a' - count the occurrences of a specific character in string 
 # "str". Indexed addressing is used to access the array elements.
 #  MAX Score: 15 points
