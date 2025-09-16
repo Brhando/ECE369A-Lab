6f80ee38f332b4ff17d9c3ca99adc34b87dde5b1
GriffithW <griffithWiele@gmail.com>
2025-09-15 23:55:14 -0700

    added code to calculate the sad for 2 1D arrays

--- Lab3/vbsme.c
+++ Lab3/vbsme.c
@@ -3,26 +3,30 @@
 #include <math.h>
 
 
-
-// int sad(int* window, int* frame, int fRow, int fCol){
-//     int sum;
-//     for(int i = 0; i < fCol, i++;){
-//         for(int j = 0; j < fRow; j++){
-//         //sum = abs(window[i] - frame[i]);
-        
-//     }     
-//     }
-//     return sum;
-// }
+//takes a 1D array for window and frame, fRow and fCol are the dimensions of the frame 2D array, wCol and wRow are the dimensions of the window, and fRowIndex and fColIndex
+//are the indices of the element in the frame that the calculation is based on
+int sad(int* window, int* frame, int fRow, int fCol, int wCol, int wRow, int fRowIndex, int fColIndex){
+    int sum;
+    int window1DIndex;
+    int frame1DIndex;
+    for(int i = 0; i < fCol; i++){
+        for(int j = 0; j < fRow; j++){
+            window1DIndex = convert2dTo1dIndex(wCol, i, j); //converts the index for the window into 2D
+            frame1DIndex = convert2DTo1DIndex(fCol, i + fRowIndex, j + fColIndex); //converts the index for the current element of the frame
+            sum += abs(window[window1DIndex] - frame[frame1DIndex]);
+    }     
+    }
+    return sum;
+}
 
 
 //i = col index, j = row index
 
 
-int convert2dTo1dIndex(int col, int i, int j){
-    int row1d = col * j;
-    int index1d = row1d + i;
-    return index1d; 
+int convert2DTo1DIndex(int col, int i, int j){
+    int row1D = col * j;
+    int index1D = row1D + i;
+    return index1D; 
 }
 
 void diagSearch(int col,int  row){
