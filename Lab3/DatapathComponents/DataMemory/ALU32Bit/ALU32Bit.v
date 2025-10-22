06ae932ba59f0438e3b9c29641edd2a674a432d7
GriffithW <149918731+GriffithW@users.noreply.github.com>
2025-10-22 14:58:05 -0700

    Update ALU32Bit.v

--- Lab3/DatapathComponents/DataMemory/ALU32Bit/ALU32Bit.v
+++ Lab3/DatapathComponents/DataMemory/ALU32Bit/ALU32Bit.v
@@ -50,54 +50,56 @@ module ALU32Bit(ALUControl, A, B, ALUResult, ConFlag, Hi, Lo);
         
         
         case (ALUControl)
+        //and: and, andi
+    4'd0: ALUResult = A & B;
+
+    //or: or, ori
+    4'd1: ALUResult = A | B;
+
     //add: add, addi, lw (lw $t0, 8($s0)   A = base memory address = $s0, B = offset = 8, ALUResult = address to load word from, $t0 is set externally using address computed in ALU ),
     //sw (sw $s0, 8($t0) A = word to store = $s0, B = offset = 8, ALUResult = address to store word in, $s0 the word that will be saved in the copmuted memory address [done externally]),
     //lb, lh, sh, sb these work the same as sw and lw, one byte or two bytes handled externally 
-    4'd0: ALUResult = A + B;
+    4'd2: ALUResult = A + B;
     
+    //xor: xor, xori
+    4'd3: ALUResult = A ^ B;
+
+    //nor: nor
+    4'd4: ALUResult = ~(A | B);
+
+    //sll: sll ( B is original number, A is shift amount )
+    4'd5: ALUResult = B << A[4:0];
+
     
     //sub: sub
-    4'd1: ALUResult = A - B;
+    4'd6: ALUResult = A - B;
     
+    //slt: slt
+    4'd7: ALUResult = (A < B) ? 32'd1 : 32'd0;
+
+    //srl: srl ( B is original number, A is shift amount )
+    4'd8: ALUResult = B >> A[4:0];
     
     //mult: mult
-    4'd2: begin 
+    4'd9: begin 
         temp64 = $signed(A) * $signed(B);
         Hi = temp64[63:32];
         Lo = temp64[31:0];
     end
-    //slt: slt
-    4'd3: ALUResult = (A < B) ? 32'd1 : 32'd0;
-    
-    //sltb: blez (A = register input, B = 1), bltz (A = register input, B = 0)
-    4'd4: ConFlag = (A < B);
     
     //sgtb: bgez (A = register input, B = -1), bgtz (A = register input, B = 0) 
-    4'd5: ConFlag = (A > B);
+    4'd10: ConFlag = (A > B);
+
+    //sltb: blez (A = register input, B = 1), bltz (A = register input, B = 0)
+    4'd11: ConFlag = (A < B);
+    
     
     //seqb: beq
-    4'd6: ConFlag = (A == B);
+    4'd12: ConFlag = (A == B);
     
     //sneb: bne
-    4'd7: ConFlag = ( A != B);
+    4'd13: ConFlag = ( A != B);
     
-    //and: and, andi
-    4'd8: ALUResult = A & B;
-    
-    //or: or, ori
-    4'd9: ALUResult = A | B;
-    
-    //nor: nor
-    4'd10: ALUResult = ~(A | B);
-    
-    //xor: xor, xori
-    4'd11: ALUResult = A ^ B;
-
-    //sll: sll ( B is original number, A is shift amount )
-    4'd12: ALUResult = B << A[4:0];
-    
-    //srl: srl ( B is original number, A is shift amount )
-    4'd13: ALUResult = B >> A[4:0];
     
     default: begin 
     ALUResult = 0; 
